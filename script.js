e2081f5f37b4dbb006b238baa4c7fc006e90fcea
Jason Vernot <jason.vernot@gmail.com>
2020-03-19 00:56:10 -0400

    added functionality to correct save button. Coded click events and local storage for AM time blocks.

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
 
-    $(".saveBtn").click(function() {
+    //Local storage for 9 AM
+    $(".saveBtn.b9").click(function() {
         console.log("click");
-        var description = $("d9").val();
-        // need to turn description into something that storage can read
-        localStorage.setItem("9:00 AM: ", description);
-        $("d9").text("9:00 AM: " + localStorage.getItem("9:00 AM:"))
+        var description = $(".d9").val();
+    
+        localStorage.setItem("9:00 AM", description);
+        
     })
 
+    $(".d9").val(localStorage.getItem("9:00 AM"));
+
+    //Local storage for 10 AM
+    $(".saveBtn.b10").click(function() {
+        console.log("click");
+        var description = $(".d10").val();
+    
+        localStorage.setItem("10:00 AM", description);
+        
+    })
+
+    $(".d10").val(localStorage.getItem("10:00 AM"));
+
+    //Local storage for 11 AM
+    $(".saveBtn.b11").click(function() {
+        console.log("click");
+        var description = $(".d11").val();
+    
+        localStorage.setItem("11:00 AM", description);
+        
+    })
+
+    $(".d11").val(localStorage.getItem("11:00 AM"));
+
+    //Local storage for 12 PM
+    $(".saveBtn.b12").click(function() {
+        console.log("click");
+        var description = $(".d12").val();
+    
+        localStorage.setItem("12:00 PM", description);
+        
+    })
+
+    $(".d12").val(localStorage.getItem("12:00 PM"));
 
 
 
